13466c0b606e108305646ff9e67525b1626030c5
Portgas·D·Asce <lupengkunmc@163.com>
2024-07-22 23:38:47 +0800

    bug: redix context may be refreed

--- src/redis_pool/redis_connection.cpp
+++ src/redis_pool/redis_connection.cpp
@@ -11,6 +11,8 @@ bool RedisConnection::connect(const struct timeval& timeout) {
     if(_ctx) {
         printf("connect error: %s\n", _ctx->errstr);
         redisFree(_ctx);
+        // 释放之后，设置为空，防止在析构时二次释放
+        _ctx = nullptr;
     } else {
         printf("connect error: connect failed\n");
     }
