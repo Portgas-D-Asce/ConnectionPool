85a0e21d4227b34de966c634be9dce9ac81c46e9
Portgas·D·Asce <lupengkunmc@163.com>
2024-07-26 04:10:59 +0800

    fix singleton bug

--- src/singleton/singleton.h
+++ src/singleton/singleton.h
@@ -6,11 +6,10 @@
 template<typename T>
 class Singleton {
 public:
-    // 如何兼容零个参数
+    // 兼容 0 个，多个任意参数，同种类型仅有一个实例，即使传递参数不同
     template<typename... Args>
     static T& get_instance(Args&&... args) {
-        static std::once_flag flag;
-        std::call_once(flag, [&]() {
+        std::call_once(_init_flag, [&]() {
             // 这块还有完美转发的必要么，lambda 表达式已经引用了？？？？？？？
             _instance = std::unique_ptr<T>(new T(std::forward<Args>(args)...));
         });
@@ -25,9 +24,15 @@ public:
     }
 private:
     static std::unique_ptr<T> _instance;
+    // 使用不同的参数构造单例，会导致多个局部静态变量，进而导致多例
+    // 故取消局部静态变量
+    static std::once_flag _init_flag;
 };
 
 template<typename T>
 std::unique_ptr<T> Singleton<T>::_instance = nullptr;
+
+template<typename T>
+std::once_flag Singleton<T>::_init_flag = {};
 
 #endif //CONNECTIONPOOL_SINGLETON_H
